8f3dabf30df9c1c2b41b0a191daca30f5728a30f
Vansh Gupta <guptavansh0224@outlook.com>
2026-09-02 00:22:21 +0530

    Increase top Sentire header logo size on Mobile and Desktop

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
-        entryFileNames: "assets/app-v12-[hash].js",
+        entryFileNames: "assets/app-v13-[hash].js",
         chunkFileNames: "assets/chunk-[name]-[hash].js",
         assetFileNames: "assets/[name]-[hash].[ext]",
         manualChunks: {
